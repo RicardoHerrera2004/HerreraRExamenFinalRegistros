b847063d1669c7fd2d27d547a3683a2187a348b9
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2025-07-10 07:19:30 -0500

    Primer Commit: Creación de la carpeta modelos con el primero modelo principal "Proyecto" y el modelo "Log" de los registros de los proyectos

---
+++ HerreraRExamenFinalRegistros/Models/Proyecto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace HerreraRExamenFinalRegistros.Models
+{
+    class Proyecto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string NombreProyecto { get; set; }
+        public string Responsable { get; set; }
+        public decimal Progreso { get; set; }
+        public int DuracionDias { get; set; }
+    }
+}
